a3dfe8ab643773fbe85c6437d92106c0006d286c
ardagencer1206 <73840764+ardagencer1206@users.noreply.github.com>
2025-10-09 11:26:31 +0300

    Update backend.py

--- backend.py
+++ backend.py
@@ -129,8 +129,8 @@ def pick_solver():
                         s.options["tmlim"] = int(MAX_SOLVE_SECONDS)
                     elif cand == "cplex":
                         s.options["timelimit"] = MAX_SOLVE_SECONDS
-                        s.options["mipgap"] = 0.99
-                        s.options["threads"] = 4
+                        s.options["mipgap"] = 0.55
+                        s.options["threads"] = 2
                 except Exception:
                     pass
                 return cand, s, False
@@ -790,6 +790,7 @@ def solve():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
 
 
 
